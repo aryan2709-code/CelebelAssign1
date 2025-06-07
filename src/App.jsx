d1599587d94ed62511675e562999772547436e5f
Aryan <bhardwaj7a@gmail.com>
2025-06-07 16:40:14 +0530

    Defined Route structure and stitched the application together

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import Form from "./components/Form"
+import SuccessPage from "./components/SuccessPage"
 function App() {
 return (
-  <div>
-    </div>
-)
+  <Router>
+    <Routes>
+      <Route path="/" element = {<Form />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
+  </Router>
+ );
 }
 
 export default App
